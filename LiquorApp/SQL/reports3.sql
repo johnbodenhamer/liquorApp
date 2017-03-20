40e9aae4e5cfae7288f4acffe33a0fd4fc50f204
ryan <ryan@192.168.1.4>
2017-03-19 21:28:19 -0500

    another sql change

--- LiquorApp/SQL/reports3.sql
+++ LiquorApp/SQL/reports3.sql
@@ -1,6 +1,6 @@
 
 
-select p.brand, sum(s.ouncesSold), count(p.brand), p.fizz
+select p.brand, sum(s.ouncesSold), count(p.brand), p.fizz, p.buzz
 from product p, sales s
 where p.productID=s.productID
 group by p.brand;
